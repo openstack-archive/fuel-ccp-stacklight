d73c7b6c2d4eeb808e04d07a39d8e73a8f384c58
Proskurin Kirill <kproskurin@mirantis.com>
2016-08-10 13:31:13 +0200

    Add ovs_vswitchd logs collecting support for heka
    Since ovs logging to stderr too and it's not OS service, I have to create a
    multidecoder for dockerlogs. Using it, I can try different decoders on each
    log string to find the right one.
    
    It's the only option avalible for us right one, since Dockerlogs plugin doesnt
    support docker containers filtering.
    
    Change-Id: I53ab2beb49e5847c3e17e443a7838b0448cb066f

---
+++ docker/heka/plugins/decoders/os_ovs_vswitchd.lua
@@ -0,0 +1,63 @@
+-- Copyright 2015-2016 Mirantis, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+require "string"
+local l      = require 'lpeg'
+l.locale(l)
+
+local patt   = require 'os_patterns'
+local utils  = require 'os_utils'
+
+local msg = {
+    Timestamp   = nil,
+    Type        = 'log',
+    Hostname    = nil,
+    Payload     = nil,
+    Pid         = nil,
+    Fields      = {},
+    Severity    = nil,
+}
+
+-- vswitchd logs looks like this:
+-- 2016-08-10T09:27:41Z|00038|connmgr|INFO|br-ex<->tcp:127.0.0.1:6633: 2 flow_mods 10 s ago (2 adds)
+
+-- Different pieces of pattern
+local sp = patt.sp
+local colon = patt.colon
+local pipe = patt.pipe
+local dash = patt.dash
+
+local p_timestamp = l.digit^-4 * dash * l.digit^-2 * dash * l.digit^-2
+local p_date = l.digit^-2 * colon * l.digit^-2 * colon * l.digit^-2
+local p_id = l.digit^-5
+local p_module = l.Cg(l.R("az")^0, "module")
+local p_severity_label = l.Cg(l.R("AZ")^0, "SeverityLabel")
+local p_message = l.Cg(patt.Message, "Message")
+
+local vswitchd_grammar = l.Ct(p_timestamp * l.S("T") * p_date * l.S("Z") * pipe * p_id * pipe * p_module * p_severity_label * pipe * p_message)
+
+
+function process_message ()
+    local log = read_message("Payload")
+    local logger = "ovs_vswitchd"
+
+    local m = vswitchd_grammar:match(log)
+    if not m then return -1 end
+
+    msg.Logger = logger
+    msg.Payload = m.Message
+    msg.Fields.severity_label = m.SeverityLabel
+    msg.Fields.module = m.module
+
+    return utils.safe_inject_message(msg)
+end
