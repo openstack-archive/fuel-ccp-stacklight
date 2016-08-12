55b1b7deb9ab686e32730778153465cc2490e697
Éric Lemoine <elemoine@mirantis.com>
2016-08-12 17:00:43 +0200

    Collect kubelet stats
    Change-Id: I0b39d416ebc4e8090a959267d6fc813ddab2674a

--- docker/hindsight/install.sh
+++ docker/hindsight/install.sh
@@ -6,9 +6,19 @@ set -e
 # Install build dependencies
 #
 
-BUILD_DEPS="git g++ make cmake"
+BUILD_DEPS="git g++ make libssl-dev"
 apt-get update
 apt-get install -y --no-install-recommends $BUILD_DEPS
+
+#
+# Install cmake 3.5.2
+# cmake 3.0.2, shipped by Debian Jessie, does not work for building the ssl
+# sandbox extension.
+# See https://github.com/mozilla-services/lua_sandbox_extensions/issues/19.
+#
+
+curl https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz | tar xz -C /tmp
+export PATH=/tmp/cmake-3.5.2-Linux-x86_64/bin:$PATH
 
 #
 # Build and install lua_sandbox
@@ -27,6 +37,7 @@ make install
 #
 # Build and install the necessary lua_sandbox extensions
 #
+
 cd /tmp
 git clone https://github.com/mozilla-services/lua_sandbox_extensions
 cd lua_sandbox_extensions
@@ -35,7 +46,7 @@ cd lua_sandbox_extensions
 git checkout 98065e7627ebf7440363ad73024968b01a1d5c53
 mkdir release
 cd release
-cmake -DCMAKE_BUILD_TYPE=release -DEXT_cjson=on -DEXT_heka=on -DEXT_lpeg=on -DEXT_socket=on ..
+cmake -DCMAKE_BUILD_TYPE=release -DEXT_cjson=on -DEXT_heka=on -DEXT_lpeg=on -DEXT_socket=on -DEXT_ssl=on ..
 make
 # make install does not work
 make packages
@@ -43,6 +54,7 @@ tar --strip-components=1 -C / -xvzf luasandbox-cjson-*-Linux.tar.gz
 tar --strip-components=1 -C / -xvzf luasandbox-heka-*-Linux.tar.gz
 tar --strip-components=1 -C / -xvzf luasandbox-lpeg-*-Linux.tar.gz
 tar --strip-components=1 -C / -xvzf luasandbox-socket-*-Linux.tar.gz
+tar --strip-components=1 -C / -xvzf luasandbox-ssl-*-Linux.tar.gz
 
 ldconfig
 
@@ -83,7 +95,7 @@ cp /tmp/hindsight/sandboxes/input/prune_input.lua /var/lib/hindsight/run/input/
 #
 # Remove build directories
 #
-rm -rf /tmp/lua_sandbox /tmp/lua_sandbox_extensions /tmp/hindsight
+rm -rf /tmp/lua_sandbox /tmp/lua_sandbox_extensions /tmp/hindsight /tmp/cmake*
 apt-get purge -y --auto-remove $BUILD_DEPS
 apt-get clean
 rm -rf /var/lib/apt/lists/*
