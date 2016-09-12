ed5934cd36b67734bb4ee1ebf6c3f046f58e240d
Éric Lemoine <elemoine@mirantis.com>
2016-09-12 12:22:14 +0000

    Add Lua code for alarming
    This commits adds Lua code for generating AFD (Anomaly and
    Fault Detection) metrics based on the evaluation of alarms.
    The Lua code was copied from the lma_collector Fuel plugin
    [*], with changes to accomodate Hindsight and the versions
    of lua_sandbox and lua_sandbox_extensions we rely on.
    
    In the future we plan to move this Lua code in its own Git
    repository. And the Hindsight Dockerfile will install the
    Lua code in the image using Debian packages.
    
    The afd_node_default_cpu_alarms.lua and
    hindsight_afd_node_default_cpu_alarms.cfg.j2 files will be
    removed. Instead the operator will configure alarms through
    a YAML file, and we will use a sidecar container for
    generating Lua tables including alarm definitions and
    corresponding plugin configuration files.
    
    [*] https://github.com/openstack/fuel-plugin-lma-collector/
    
    Change-Id: If182c3a6453f7bf8b72f03af56a14ace109eaa68

---
+++ docker/hindsight/modules/utils.lua
@@ -0,0 +1,46 @@
+-- Copyright 2015-2016 Mirantis, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local cjson = require 'cjson'
+
+local inject_message = inject_message
+local read_message = read_message
+local string = string
+local pcall = pcall
+
+local M = {}
+setfenv(1, M) -- Remove external access to contain everything in the module
+
+-- Encode a Lua variable as JSON without raising an exception if the encoding
+-- fails for some reason (for instance, the encoded buffer exceeds the sandbox
+-- limit)
+function safe_json_encode(v)
+    local ok, data = pcall(cjson.encode, v)
+    if not ok then
+        return
+    end
+    return data
+end
+
+-- Call inject_message() wrapped by pcall()
+function safe_inject_message(msg)
+    local ok, err_msg = pcall(inject_message, msg)
+    if not ok then
+        return -1, err_msg
+    else
+        return 0
+    end
+end
+
+return M
