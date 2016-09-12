00b128ce389c0d73705379a9c1fc9c80313e5cb1
Éric Lemoine <elemoine@mirantis.com>
2016-09-12 12:22:14 +0000

    Add Lua code for alarming
    This commits adds Lua code for generating AFD (Anomaly and
    Fault Detection) metrics based on the evaluation of alarms.
    The Lua code was copied from the lma_collector Fuel plugin
    [*], with changes to accomodate Hindsight and the versions
    of lua_sandbox and lua_sandbox_extensions we rely on.
    
    In the future we plan to move this Lua code in its own Git
    repository. And the Hindsight Dockerfile will install the
    Lua code in the image using Debian packages.
    
    The cpu_alarms.lua and hindsight_cpu_alarms.cfg.j2 file will
    be removed. Instead the operator will configure alarms
    through a YAML file, and we will use a sidecar container for
    generating Lua tables including alarm definitions and the
    corresponding plugin configuration files.
    
    [*] https://github.com/openstack/fuel-plugin-lma-collector/
    
    Change-Id: If182c3a6453f7bf8b72f03af56a14ace109eaa68

---
+++ docker/hindsight/modules/constants.lua
@@ -0,0 +1,78 @@
+-- Copyright 2015-2016 Mirantis, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local M = {}
+setfenv(1, M) -- Remove external access to contain everything in the module
+
+-- The status values were chosen to match with the Grafana constraints:
+-- OKAY => green
+-- WARN & UNKW => orange
+-- CRIT & DOWN => red
+OKAY=0
+WARN=1
+UNKW=2
+CRIT=3
+DOWN=4
+
+local STATUS_LABELS = {
+    [OKAY]='OKAY',
+    [WARN]='WARN',
+    [UNKW]='UNKNOWN',
+    [CRIT]='CRITICAL',
+    [DOWN]='DOWN'
+}
+
+function status_label(v)
+    return STATUS_LABELS[v]
+end
+
+local STATUS_WEIGHTS = {
+    [UNKW]=0,
+    [OKAY]=1,
+    [WARN]=2,
+    [CRIT]=3,
+    [DOWN]=4
+}
+
+function max_status(val1, val2)
+    if not val1 then
+        return val2
+    elseif not val2 then
+        return val1
+    elseif STATUS_WEIGHTS[val1] > STATUS_WEIGHTS[val2] then
+        return val1
+    else
+        return val2
+    end
+end
+
+function compare_threshold(value, op, threshold)
+    local rule_matches = false
+    if op == '==' or op == 'eq' then
+        rule_matches = value == threshold
+    elseif op == '!=' or op == 'ne' then
+        rule_matches = value ~= threshold
+    elseif op == '>=' or op == 'gte' then
+        rule_matches = value >= threshold
+    elseif op == '>' or op == 'gt' then
+        rule_matches = value > threshold
+    elseif op == '<=' or op == 'lte' then
+        rule_matches = value <= threshold
+    elseif op == '<' or op == 'lt' then
+        rule_matches = value < threshold
+    end
+    return rule_matches
+end
+
+return M
